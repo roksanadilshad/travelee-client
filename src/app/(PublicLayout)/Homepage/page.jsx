3891c842dfa6850e126af9607cb178bd9ce15a19
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2026-02-15 16:50:44 +0600

    [CREATED]:home page banner section

---
+++ src/app/(PublicLayout)/Homepage/page.jsx
@@ -0,0 +1,12 @@
+import Banner from '@/components/Home/Banner';
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default HomePage;
--- src/app/(PublicLayout)/page.js
+++ src/app/(PublicLayout)/page.js
@@ -1,11 +1,10 @@
+import HomePage from "./Homepage/page";
 
 
 export default function Home() {
   return (
    <div>
-    <h1>
-      This is a travel management site 101
-    </h1>
+     <HomePage></HomePage>
    </div>
   );
 }
