6eef66d2b583fee6d728d9463069b818004187a3
roky18 <rokymax66@gmail.com>
2026-02-19 07:28:18 +0600

    KAN-12[ADDED]Destination Details Roky

--- src/app/(PublicLayout)/destinations/[id]/page.jsx
+++ src/app/(PublicLayout)/destinations/[id]/page.jsx
@@ -1,4 +1,6 @@
 import DestinationDetailsCard from "@/components/Share/cards/DestinationDetailsCard";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 const DestinationDetailsPage = async ({ params }) => {
   const { id } = await params;
@@ -10,13 +12,32 @@ const DestinationDetailsPage = async ({ params }) => {
   const destination = await res.json();
 
   if (!destination || destination.error) {
-    return <div className="text-center mt-16 text-gray-500">Destination not found</div>;
+    return (
+      <div className="text-center mt-16 text-gray-500">
+        Destination not found
+      </div>
+    );
   }
 
   return (
-    <div className="max-w-11/12 mx-auto my-12 bg-white shadow-lg rounded-xl overflow-hidden">
-     <DestinationDetailsCard destination={destination}/>
-    
+    <div className="w-11/12 mx-auto">
+      <div className="mt-12 bg-white shadow-lg rounded-xl overflow-hidden">
+        {/* Destination Details Card */}
+
+        <DestinationDetailsCard destination={destination} />
+      </div>
+
+      {/* back to Destinations btn */}
+
+      <span className="flex justify-center my-8">
+        <Link href="/destinations">
+          <button className="mt-4 bg-orange-500 text-white px-4 py-2 flex items-center rounded-lg hover:bg-amber-700 transition">
+            <ArrowLeft className="w-5 h-5" /> Destinations
+          </button>
+        </Link>
+      </span>
+
+
     </div>
   );
 };
