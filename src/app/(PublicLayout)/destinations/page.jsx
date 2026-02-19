616d1ff168b0cd93ead5b42018c4ade9429be62a
roky18 <rokymax66@gmail.com>
2026-02-20 00:45:06 +0600

    KAN-10[ADDED]:DestinationsPagination

--- src/app/(PublicLayout)/destinations/page.jsx
+++ src/app/(PublicLayout)/destinations/page.jsx
@@ -1,26 +1,35 @@
 import DestinationCard from "@/components/Share/cards/DestinationCard";
+import DestinationsPagination from "@/components/Share/DestinationsPagination";
 import FilterAndSearch from "@/components/Share/FilterAndSearch";
 
+// searchParams
 export default async function DestinationsPage({ searchParams }) {
-  // searchParams is a built-in Next.js feature to get URL queries
+  // query params
   const city = (await searchParams).city || "";
+  const page = parseInt((await searchParams).page) || 1;
+  const limit = 9;
 
-  // Fetching from your Node.js backend
-  const res = await fetch(`http://localhost:500/destinations?city=${city}`, {
-    cache: "no-store",
-  });
-  const destinations = await res.json();
+  // Fetching
+  const res = await fetch(
+    `http://localhost:500/destinations?city=${city}&page=${page}&limit=${limit}`,
+    {
+      cache: "no-store",
+    },
+  );
 
+  const { data: destinations, totalPages } = await res.json();
   return (
     <div className="w-11/12 mx-auto">
       {/* Title---->>> */}
       <h1 className="text-4xl flex justify-center font-bold mb-8 my-4">
         {city ? `Showing results for "${city}"` : "All Destinations"}
       </h1>
+        <span className="flex justify-end font-semibold text-gray-500 mr-6">Found: ( {destinations.length} )</span>
 
       <div>
         {/* Main Content---->>> */}
         <main className="grid grid-cols-1 md:grid-cols-12  gap-3 p-3">
+          
           {/* search & Filter---->> */}
           <aside className="col-span-1 md:col-span-3 lg:col-span-2 bg-amber-100">
             <FilterAndSearch></FilterAndSearch>
@@ -39,7 +48,12 @@ export default async function DestinationsPage({ searchParams }) {
                 <p>No destinations found for {city}.</p>
               )}
             </div>
-          </div>
+            <DestinationsPagination
+              totalPages={totalPages}
+              page={page}
+              city={city}
+            ></DestinationsPagination>
+          </div>{" "}
         </main>
       </div>
     </div>
