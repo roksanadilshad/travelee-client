e97ab978a8d73b3eb1fb2eddce1f94a48796b98e
ashadulislam6156rs <ashadulislam6156rs@gamil.com>
2026-02-23 00:25:24 +0600

    [ADDED and MODIFY]: added proxy and implement  if a user tries to access /itinerary and /dashboard  without being logged in, they are redirected to /login and modify the google login error

---
+++ src/components/Auth/ForgotPassword.jsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React, { useState } from "react";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+const ForgotPasswordFun = () => {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setMessage("");
+    setError("");
+
+    try {
+      const res = await fetch("http://localhost:500/forgot-password", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setMessage("If this email exists, a reset link has been sent.");
+        setEmail("");
+      } else {
+        setError(data.message || "Something went wrong");
+      }
+    } catch (err) {
+      setError("Server error. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-5xl mx-auto p-6 border rounded-lg shadow-md space-y-4"
+    >
+      <Field>
+        <FieldLabel htmlFor="fieldgroup-email">Enter Email</FieldLabel>
+
+        <Input
+          id="fieldgroup-email"
+          type="email"
+          placeholder="name@example.com"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <FieldDescription>Forgot your password?</FieldDescription>
+      </Field>
+
+      <button
+        type="submit"
+        disabled={loading}
+        className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+      >
+        {loading ? "Sending..." : "Send Reset Link"}
+      </button>
+
+      {message && (
+        <p className="text-green-600 text-sm text-center">{message}</p>
+      )}
+
+      {error && <p className="text-red-600 text-sm text-center">{error}</p>}
+    </form>
+  );
+};
+
+export default ForgotPasswordFun;
