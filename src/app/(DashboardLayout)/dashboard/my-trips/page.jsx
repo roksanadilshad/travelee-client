af4b352a836a07e9ff9fca8bef205b3686204777
Roksana Dilshad <roksanadilshad@gmail.com>
2026-03-11 23:37:17 +0600

    [FIXED]: fixed error from itinerary [id]

--- src/app/(DashboardLayout)/dashboard/my-trips/page.jsx
+++ src/app/(DashboardLayout)/dashboard/my-trips/page.jsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
 import Swal from "sweetalert2";
 import BookingsList from "@/components/MyTrips/BookingsList";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function MyTripsPage() {
   const { data: session } = useSession();
@@ -13,6 +14,7 @@ export default function MyTripsPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get("q")?.toLowerCase() || "";
   const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://travelee-server.vercel.app";
+  const {token} = useAuth()
 
   useEffect(() => {
     if (session?.user?.email) {
